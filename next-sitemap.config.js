f7ba338b8a7797ff4470b7c8144094f88ebdbd35
Bugra Ayar <bugraayar@Bugras-MacBook-Pro.local>
2025-04-19 19:02:30 +0300

    🗺️ Sitemap ve robots.txt dosyaları eklendi

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://kreatifguru.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'weekly',
+    priority: 0.7,
+  };
